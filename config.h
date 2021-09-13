e802bd3f997fc4250c9df9e440a8d80c64846fd5
Joel Linn <jl@conductive.de>
2021-09-13 20:49:03 +0200

    Generate configs
    - Rename old Windows and Linux
    - Add Android x86_64 and aarch64

--- config.h
+++ config.h
@@ -1,7 +1,15 @@
-#if defined(_WIN32)
-#include "config_win.h"
-#elif defined(__linux)
-#include "config_lin.h"
+#if defined(WIN32) || defined(_WIN32)
+#include "config_windows_x86_64.h"
+#elif defined(__gnu_linux__)
+#include "config_linux_x86_64.h"
+#elif defined(__ANDROID__)
+#if defined(__aarch64__)
+#include "config_android_aarch64.h"
+#elif defined(__amd64__)
+#include "config_android_x86_64.h"
+#elif
+#error "android arch unsupported"
+#endif
 #else
 #error "no config"
 #endif
