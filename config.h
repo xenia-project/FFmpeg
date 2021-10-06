c1a4de883a3325ddac88bb7378b273b464cf64be
Conrad Kramer <conrad@conradkramer.com>
2021-10-06 14:31:23 -0700

    Add macOS configurations

--- config.h
+++ config.h
@@ -1,7 +1,13 @@
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+#endif
+
 #if defined(WIN32) || defined(_WIN32)
 #include "config_windows_x86_64.h"
 #elif defined(__gnu_linux__)
 #include "config_linux_x86_64.h"
+#elif TARGET_OS_MAC
+#include "config_macosx_x86_64.h"
 #elif defined(__ANDROID__)
 #if defined(__aarch64__)
 #include "config_android_aarch64.h"
