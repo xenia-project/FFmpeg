8cd83b9885c28efe7488398cc19720089f84fc90
Triang3l <triang3l@yandex.ru>
2021-10-29 23:40:56 +0300

    Premake generator: change Android_ to Android- platforms

--- libavutil/premake5.lua
+++ libavutil/premake5.lua
@@ -195,14 +195,14 @@ project("libavutil")
 
   -- libavutil/aarch64/Makefile:
   --   OBJS:
-  filter({"platforms:Android_ARM64"})
+  filter({"platforms:Android-ARM64"})
   files({
     "aarch64/cpu.c",
     "aarch64/float_dsp_init.c",
   })
   filter({})
   --   NEON-OBJS:
-  filter({"platforms:Android_ARM64"})
+  filter({"platforms:Android-ARM64"})
   files({
     "aarch64/float_dsp_neon.S",
   })
@@ -210,7 +210,7 @@ project("libavutil")
 
   -- libavutil/x86/Makefile:
   --   OBJS:
-  filter({"platforms:Android_x86_64 or platforms:Linux or platforms:Windows"})
+  filter({"platforms:Android-x86_64 or platforms:Linux or platforms:Windows"})
   files({
     "x86/cpu.c",
     "x86/fixed_dsp_init.c",
